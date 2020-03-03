57f31404979c4c460aa63650a0f55dfa8bd5c1bf
Li Yue <rmapyli@ucl.ac.uk>
2020-03-03 09:15:22 +0000

    refactoring4: make sure three random points are different

--- clustering.py
+++ clustering.py
@@ -18,10 +18,10 @@ class Points:
   def cluster(self, n=10):
     k = 3
     ps = self.read_data()
-    m = [ps[randrange(len(ps))], ps[randrange(len(ps))], ps[randrange(len(ps))]]
-    alloc = []
+    m = sample(ps, k)
     n = 0
     while n<10:
+      alloc = []  
       for i in range(len(ps)):
         p = ps[i]
         d = [None] * k
@@ -30,14 +30,16 @@ class Points:
         d[2] = sqrt((p[0]-m[2][0])**2 + (p[1]-m[2][1])**2)
         min_index = d.index(min(d))
         alloc.append(min_index)
+      alloc_ps = [None]*k  
       for i in range(k):
-        alloc_ps = [None]*k
-        alloc_ps[i] = [p for j, p in enumerate(ps) if alloc[j] == i]
+        alloc_ps[i] = []
+        for j in range(len(ps)):
+            alloc_ps[i] = [p for j, p in enumerate(ps) if alloc[j] == i]
         new_mean = (sum([a[0] for a in alloc_ps[i]]) / len(alloc_ps[i]), sum([a[1] for a in alloc_ps[i]]) / len(alloc_ps[i]))
         m[i] = new_mean
       n=n+1
             
-      for i in range(k):
+    for i in range(k):
         alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
         print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_ps)) + " points.")
             
