e2d30176c35bbf5e4850c3087b28db6564855de8
Li Yue <rmapyli@ucl.ac.uk>
2020-03-03 01:17:25 +0000

    refactoring3: seperate code concepts into functions

--- clustering.py
+++ clustering.py
@@ -1,32 +1,45 @@
 from math import *
 from random import *
 
-k=3
 
-file = 'data/samples.csv'
-with open(file) as fn:
-  lines = fn.readlines()
-  ps = []
-  for line in lines: ps.append(tuple(map(float, line.strip().split(','))))
+class Points:
+  
+  def __init__(self, file):
+    self.filename = file
+    
+  def read_data(self):
+    with open(self.filename) as fn:
+      lines = fn.readlines()
+      ps = []
+      for line in lines: 
+        ps.append(tuple(map(float, line.strip().split(','))))
+      return ps
 
-m=[ps[randrange(len(ps))], ps[randrange(len(ps))], ps[randrange(len(ps))]]
-
-alloc=[None]*len(ps)
-n=0
-while n<10:
-  for i in range(len(ps)):
-    p=ps[i]
-    d=[None] * k
-    d[0]=sqrt((p[0]-m[0][0])**2 + (p[1]-m[0][1])**2)
-    d[1]=sqrt((p[0]-m[1][0])**2 + (p[1]-m[1][1])**2)
-    d[2]=sqrt((p[0]-m[2][0])**2 + (p[1]-m[2][1])**2)
-    alloc[i]=d.index(min(d))
-  for i in range(k):
-    alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
-    new_mean=(sum([a[0] for a in alloc_ps]) / len(alloc_ps), sum([a[1] for a in alloc_ps]) / len(alloc_ps))
-    m[i]=new_mean
-  n=n+1
-
-for i in range(k):
-  alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
-  print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_ps)) + " points.")
+  def cluster(self, n=10):
+    k = 3
+    ps = self.read_data()
+    m = [ps[randrange(len(ps))], ps[randrange(len(ps))], ps[randrange(len(ps))]]
+    alloc = []
+    n = 0
+    while n<10:
+      for i in range(len(ps)):
+        p = ps[i]
+        d = [None] * k
+        d[0] = sqrt((p[0]-m[0][0])**2 + (p[1]-m[0][1])**2)
+        d[1] = sqrt((p[0]-m[1][0])**2 + (p[1]-m[1][1])**2)
+        d[2] = sqrt((p[0]-m[2][0])**2 + (p[1]-m[2][1])**2)
+        min_index = d.index(min(d))
+        alloc.append(min_index)
+      for i in range(k):
+        alloc_ps = [None]*k
+        alloc_ps[i] = [p for j, p in enumerate(ps) if alloc[j] == i]
+        new_mean = (sum([a[0] for a in alloc_ps[i]]) / len(alloc_ps[i]), sum([a[1] for a in alloc_ps[i]]) / len(alloc_ps[i]))
+        m[i] = new_mean
+      n=n+1
+            
+      for i in range(k):
+        alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
+        print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_ps)) + " points.")
+            
+points = Points('data/samples.csv')
+points.cluster()
