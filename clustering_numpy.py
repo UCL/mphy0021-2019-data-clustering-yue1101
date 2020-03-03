c99b19a97f4bdd753434ae84b1165232cddf92bf
Li Yue <rmapyli@ucl.ac.uk>
2020-03-03 11:44:34 +0000

    modified version of using numpy, directly using functions rather than class

--- clustering_numpy.py
+++ clustering_numpy.py
@@ -4,49 +4,42 @@ import numpy as np
 from argparse import ArgumentParser
 
 
-class Points2:
-  def __init__(self, file):
-    self.filename = file
-  def read_data(self):
-    with open(self.filename) as fn:
-        ps = np.loadtxt(fn,delimiter=",",skiprows=0) 
-    return ps
-  def cluster(self, n=10):
-    k = 3
-    ps = self.read_data()
-    rand_arr = np.arange(ps.shape[0])
-    np.random.shuffle(rand_arr)
-    m = ps[rand_arr[0:k]]    
-    n = 0
-    num = [None]*k
-    while n<10:     
-      d = np.zeros([len(ps),k])
-      for i in range(k):
-        d[:,i] = ((ps[:,0]-m[i,0])**2+ (ps[:,1]-m[i,1])**2 )**0.5
-      alloc = np.argmin(d, axis=1)
+def read_data(name):
+  with open(name) as fn:
+    ps = np.loadtxt(fn,delimiter=",",skiprows=0) 
+  return ps
+
+def cluster(ps, N):
+  k = 3
+  rand_arr = np.arange(ps.shape[0])
+  np.random.shuffle(rand_arr)
+  m = ps[rand_arr[0:k]]    
+  n = 0
+  num = [None]*k
+  while n<N:     
+    d = np.zeros([len(ps),k])
+    for i in range(k):
+      d[:,i] = ((ps[:,0]-m[i,0])**2+ (ps[:,1]-m[i,1])**2 )**0.5
+    alloc = np.argmin(d, axis=1)
       
-      for i in range(k):
-        itemindex = np.argwhere(alloc == i).squeeze() 
-        ps_i = ps[itemindex,:]
-        m[i] = ps_i.mean(axis=0)
-        num[i] = ps_i.shape[0] 
-      n=n+1
+    for i in range(k):
+      itemindex = np.argwhere(alloc == i).squeeze() 
+      ps_i = ps[itemindex,:]
+      m[i] = ps_i.mean(axis=0)
+      num[i] = ps_i.shape[0] 
+    n=n+1
     
-    for i in range(3):
-      print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(num[i]) + " points.") 
+  for i in range(3):
+    print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(num[i]) + " points.") 
       
-def process():          
+def process():    
   parser = ArgumentParser(
     description='Cluster points into 3 classes')
   parser.add_argument('data')
-  parser.add_argument('--iter', help='iteration time for clustering')
+  parser.add_argument('--iters',  type=int, help='iteration times for clustering',default=10)
   args = parser.parse_args()
-    
-  points = Points2(args.data)
-  if args.iter:
-    points.cluster(args.iter)
-  else:
-    points.cluster()
+  ps = read_data(args.data)  
+  cluster(ps,args.iters)
 
 if __name__ == "__main__":
   process()
